7593470e7c2c98f56d785361609d059436c36b86
Yudhishthra Sugumaran <66850474+0xYudhishthra@users.noreply.github.com>
2024-11-16 14:24:20 +0700

    refactor: Update _app.tsx to include @coinbase/onchainkit and @rainbow-me/rainbowkit dependencies and configure providers

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,16 @@
 import "../styles/globals.css";
+import "@coinbase/onchainkit/styles.css";
 import "@rainbow-me/rainbowkit/styles.css";
+
+import { OnchainKitProvider } from "@coinbase/onchainkit";
+import { RainbowKitProvider } from "@rainbow-me/rainbowkit";
 import type { AppProps } from "next/app";
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { WagmiProvider } from "wagmi";
-import { RainbowKitProvider } from "@rainbow-me/rainbowkit";
 
 import { config } from "../wagmi";
+import { baseSepolia } from "wagmi/chains";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 
@@ -19,20 +23,26 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <WagmiProvider config={config}>
       <QueryClientProvider client={client}>
-        <RainbowKitProvider>
-          <LocalizationProvider dateAdapter={AdapterDayjs}>
-            <main className="min-w-full flex min-h-screen flex-col">
-              <Navbar />
-              <div className="flex-1 px-4 md:px-8 lg:px-16 xl:px-24 ">
-                <Component {...pageProps} />
-              </div>
+        <OnchainKitProvider
+          // @ts-ignore
+          chain={baseSepolia}
+          apiKey={process.env.NEXT_PUBLIC_ONCHAINKIT_API_KEY}
+        >
+          <RainbowKitProvider modalSize="compact">
+            <LocalizationProvider dateAdapter={AdapterDayjs}>
+              <main className="min-w-full flex min-h-screen flex-col">
+                <Navbar />
+                <div className="flex-1 px-4 md:px-8 lg:px-16 xl:px-24 ">
+                  <Component {...pageProps} />
+                </div>
 
-              <div className="px-4 md:px-8 lg:px-16 xl:px-24">
-                <Footer />
-              </div>
-            </main>
-          </LocalizationProvider>
-        </RainbowKitProvider>
+                <div className="px-4 md:px-8 lg:px-16 xl:px-24">
+                  <Footer />
+                </div>
+              </main>
+            </LocalizationProvider>
+          </RainbowKitProvider>
+        </OnchainKitProvider>
       </QueryClientProvider>
     </WagmiProvider>
   );
