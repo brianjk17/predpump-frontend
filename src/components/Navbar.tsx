44f4d6016150db1ad997b92664d6693545fde33e
Yudhishthra Sugumaran <66850474+0xYudhishthra@users.noreply.github.com>
2024-11-16 14:24:09 +0700

    refactor: Update Navbar.tsx to include @coinbase/onchainkit dependencies and configure wallet components

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,23 @@
-import { ConnectButton } from "@rainbow-me/rainbowkit";
 import React from "react";
 import useToken from "../hooks/useToken";
 import Link from "next/link";
+import {
+  ConnectWallet,
+  Wallet,
+  WalletDropdown,
+  WalletDropdownBasename,
+  WalletDropdownFundLink,
+  WalletDropdownLink,
+  WalletDropdownDisconnect,
+} from "@coinbase/onchainkit/wallet";
+import {
+  Address,
+  Avatar,
+  Name,
+  Identity,
+  EthBalance,
+} from "@coinbase/onchainkit/identity";
+import { color } from "@coinbase/onchainkit/theme";
 
 export default function Navbar() {
   const { userBalance } = useToken();
@@ -12,8 +28,30 @@ export default function Navbar() {
       <div className=" flex flex-row justify-center items-center gap-5 text-teal-300">
         <Link href="/profile">[View Profile]</Link>
         <Link href="/create">[Create Bets]</Link>
-        <div>[{(Number(userBalance)/(10**18)).toLocaleString()} USDC]</div>
-        <ConnectButton showBalance={false} />
+        <div>[{(Number(userBalance) / 10 ** 18).toLocaleString()} USDC]</div>
+        <Wallet>
+          <ConnectWallet withWalletAggregator>
+            <Avatar className="h-6 w-6" />
+            <Name />
+          </ConnectWallet>
+          <WalletDropdown>
+            <Identity className="px-4 pt-3 pb-2" hasCopyAddressOnClick>
+              <Avatar />
+              <Name />
+              <Address />
+              <EthBalance />
+            </Identity>
+            <WalletDropdownBasename />
+            <WalletDropdownLink icon="wallet" href="https://keys.coinbase.com">
+              Wallet
+            </WalletDropdownLink>
+            <WalletDropdownLink href="https://keys.coinbase.com">
+              Settings
+            </WalletDropdownLink>
+            <WalletDropdownFundLink />
+            <WalletDropdownDisconnect />
+          </WalletDropdown>
+        </Wallet>
       </div>
     </div>
   );
